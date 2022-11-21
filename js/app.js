988dd9795fc8bb41cab91ee7fecaff323077d093
Dennis Guan <i0fear@yahoo.com>
2022-11-21 15:26:34 -0500

    Techdegree project 5

---
+++ js/app.js
@@ -0,0 +1,9 @@
+baguetteBox.run('.gallery');
+const search = new Filter('search', 'data-caption');
+
+
+// const Search = document.getElementById('Search');
+// search.addEventListener('keyup', event=>{
+//     let currentValue=event.target.textContent.toLowerCase();
+    
+// });
